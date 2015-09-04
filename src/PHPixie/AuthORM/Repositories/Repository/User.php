1595b16b0ce91a5f891a73a33acf05d0c362512c
Dracony <draconyster@gmail.com>
2015-09-04 03:59:18 +0200

    orm auth wrappers

--- src/PHPixie/AuthORM/Repositories/Repository/User.php
+++ src/PHPixie/AuthORM/Repositories/Repository/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PHPixie\AuthORM\Repositories\Repositorys;
+namespace PHPixie\AuthORM\Repositories\Repository;
 
 abstract class User extends    \PHPixie\ORM\Wrappers\Type\Database\Entity
                     implements \PHPixie\Auth\Repositories\Repository\User
--- src/PHPixie/AuthORM/Repositories/Type/Login/User.php
+++ src/PHPixie/AuthORM/Repositories/Type/Login/User.php
@@ -5,5 +5,5 @@ namespace PHPixie\AuthORM\Repositories\Type\Login;
 abstract class User extends    \PHPixie\AuthORM\Repositories\Repository\User
                     implements \PHPixie\Auth\Repositories\Repository\Type\Login\User
 {
-    abstract protected function passwordHash();
+    abstract public function passwordHash();
 }
