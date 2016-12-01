602723be57413fafcaec1b3162959f6dbfd0ecb3
WebParishop <web_dev@parisclub.ru>
2016-12-01 15:10:02 +0400

    PHPDocs

--- src/PHPixie/AuthORM/Repositories/Repository.php
+++ src/PHPixie/AuthORM/Repositories/Repository.php
@@ -5,6 +5,10 @@ namespace PHPixie\AuthORM\Repositories;
 abstract class Repository extends    \PHPixie\ORM\Wrappers\Type\Database\Repository
                           implements \PHPixie\Auth\Repositories\Repository
 {
+    /**
+     * @param $id
+     * @return Type\Login\User
+     */
     public function getById($id)
     {
         return $this->query()->in($id)->findOne();
